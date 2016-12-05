1086d05838add206b6c20a6817098c660e4ea98a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-12-05 16:53:28 -0500

    Aberdeen Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -197,16 +197,9 @@ function renderJobDetails(container, template, collection){
             var store_details = getStoreDetailsByID(val.jobable_id);
             val.store_detail_btn = store_details.slug;
             val.store_name = store_details.name;
-            if (store_details.store_front_url_abs.indexOf('missing.png') > -1){
-                // val.image_url = "//codecloud.cdn.speedyrails.net/sites/57f66e416e6f6465fe050000/image/jpeg/1446753494000/Dixie_default.jpg";
-            }
-            else{
-                val.image_url = store_details.store_front_url_abs;
-            }
         }
         else{
-            val.store_name = "Upper Canada Mall";
-            // val.image_url = "//codecloud.cdn.speedyrails.net/sites/57f66e416e6f6465fe050000/image/jpeg/1446753494000/Dixie_default.jpg";
+            val.store_name = "Aberdeen Mall Kamloops";
         }
         
         var show_date = moment(val.show_on_web_date);
